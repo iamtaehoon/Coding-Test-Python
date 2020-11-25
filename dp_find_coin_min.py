1859ffb9266f377c6eaf64a8089aaa7c0307f497
Taehoon Kim <67636607+iamtaehoon@users.noreply.github.com>
2020-11-25 16:56:02 +0900

    Update and rename dp_coin_min.py to dp_find_coin_min.py

--- dp_find_coin_min.py
+++ dp_find_coin_min.py
@@ -1,3 +1,23 @@
+#바텀업 방식으로 풀었다. 
+n,m = map(int,input().split())
+coin_type = []
+for _ in range(n):
+    coin_type.append(int(input()))
+
+d = [10001] * (m+1)
+
+d[0] = 0
+for i in range(n):
+    for j in range(coin_type[i],m+1):
+        if d[j-coin_type[i]] != 10001:
+            d[j] = min(d[j],d[j-coin_type[i]]+1)
+
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
+
+'''
 #역시나 최단거리찾기라 생각하고 bfs로 풀었음. dp로 하는거 그대로 따라하고 코드 여기 기
 
 from collections import deque
@@ -25,3 +45,4 @@ if x<0:
     print(-1)
 else:
     print(cnt)
+'''
